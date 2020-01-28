5018c977165e99b6abd9324e4a5d8624dc99e186
Vlad-7onoff <53347929+Vlad-7onoff@users.noreply.github.com>
2020-01-28 21:54:46 +0700

    Сделал рефакторинг

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _targetRadius;
+    [SerializeField] private float _speed;
+
+    private Vector3 _target;
+
+    private void Start()
+    {
+        _target = GetNextTarget();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        if (transform.position == _target)
+        {
+            _target = GetNextTarget();
+        }
+    }
+
+    private Vector3 GetNextTarget()
+    {
+        return Random.insideUnitCircle * _targetRadius;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private float _time;
+
+    public event UnityAction<Enemy> EnemyKilled;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(0, _speed * Time.deltaTime, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0, -_speed * Time.deltaTime, 0);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-_speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+    }
+
+    private void PickUpSpeedBoost(float speedMultyplier, float accelerationTime)
+    {
+        _speed *= speedMultyplier;
+        _time = accelerationTime;
+        StartCoroutine(TimerDecrease(speedMultyplier));
+    }
+
+    public void OnCollision(Enemy enemy)
+    {
+        EnemyKilled?.Invoke(enemy);
+        Destroy(enemy.gameObject);
+    }
+
+    private IEnumerator TimerDecrease(float speedMultyplier)
+    {
+        _time -= Time.deltaTime;
+        if (_time < 0)
+        {
+            _speed /= speedMultyplier;
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+}
