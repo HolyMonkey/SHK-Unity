5773a05500b7150e3c785ab0e7c5dc7180b775e6
Velepesh <62386083+Velepesh@users.noreply.github.com>
2020-09-05 21:09:55 +0300

    Improvement e script
    Name changed to Enemy

---
+++ Assets/Group1/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Vector3 _target;
+
+    private void Start()
+    {
+        _target = Random.insideUnitCircle * 4;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        if (transform.position == _target)
+            _target = Random.insideUnitCircle * 4    ;
+    }
+}
