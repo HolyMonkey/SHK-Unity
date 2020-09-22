eb49af41e6643adba933b2e9dfd880363db2ad99
Oleg Anohin <oanohin@outlook.com>
2020-09-22 22:27:44 +0400

    Refactoring Code 1

---
+++ Assets/Group1/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Player : MonoBehaviour
+{
+  [SerializeField] private float _circleRadius = 0.2f;
+
+  private CircleCollider2D _collider2D;
+  private Rigidbody2D _rigidbody2D;
+
+  private void Start()
+  {
+    _collider2D = GetComponent<CircleCollider2D>();
+    _rigidbody2D = GetComponent<Rigidbody2D>();
+    _rigidbody2D.isKinematic = true;
+    _collider2D.radius = _circleRadius;
+  }
+}
---
+++ Assets/Group1/Scripts/PlayerMover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+  [SerializeField] private float _speed = 4;
+  [SerializeField] private bool _isTimeSpeedUp;
+  [SerializeField] private float _fastSpeedTime = 2f;
+  [SerializeField] private float _SpeedDown = 2f;
+  [SerializeField] private float _speedDown = 2f;
+  [SerializeField] private float _speedUp = 2f;
+
+  private void Start()
+  {
+    if (_isTimeSpeedUp)
+        StartCoroutine(SetPlayerMoveFaster());
+  }
+
+  private void Update()
+  {
+    float vertical = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+    float horizontal = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+
+    transform.Translate(horizontal, vertical, 0);
+  }
+
+  public void ReceiveMessage(GameObject elem)
+  {
+    if (elem.GetComponent<Speed>() != null)
+      StartCoroutine(SetPlayerMoveFaster());
+
+  }
+
+  private IEnumerator SetPlayerMoveFaster()
+  {
+    float elapsedTime = _fastSpeedTime;
+    _speed *= _speedUp;
+
+    while (elapsedTime > 0)
+    {
+      elapsedTime -= Time.deltaTime;
+      yield return null;
+    }
+
+    _speed /= _speedDown;
+  }
+}
