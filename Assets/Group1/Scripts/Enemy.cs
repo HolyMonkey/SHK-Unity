41f5a54fcd91e9d0e3e86e01ff06a187223d8639
mumuruza <53407099+mumuruza@users.noreply.github.com>
2020-12-16 19:19:19 +0100

    rename enemySpeed

--- Assets/Group1/Scripts/Enemy.cs
+++ Assets/Group1/Scripts/Enemy.cs
@@ -6,7 +6,7 @@ using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float enemySpeed = 2;
+    [SerializeField] private float moveSpeed = 2;
     [SerializeField] private float moveRange = 4;
 
     private Vector3 target;
@@ -20,7 +20,7 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target, enemySpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
         if (transform.position == target)
             PickNewTarget();
     }
