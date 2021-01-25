3c33d76ba9c0729a3c101b3d98c5397ed987be09
flangedmace <alex.solov.04@gmail.com>
2021-01-25 20:17:30 +0300

    Init unity project

--- Assets/Group1/Scripts/Enemy.cs
+++ Assets/Group1/Scripts/Enemy.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(EnemyMover))]
 public class Enemy : MonoBehaviour
 {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out Player player))
+        {
+            GetComponentInParent<EnemyOwner>().AddKilledEnemy();
 
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Group1/Scripts/EnemyOwner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyOwner : MonoBehaviour
+{
+    [SerializeField] private GameObject _menu;
+
+    private int _killedEnemies = 0;
+    private int _maxEnemiesNumber;
+
+    private void Start()
+    {
+        _maxEnemiesNumber = GetComponentsInChildren<Enemy>().Length;
+    }
+
+    public void AddKilledEnemy()
+    {
+        _killedEnemies++;
+
+        if (_killedEnemies >= _maxEnemiesNumber)
+        {
+            _menu.SetActive(true);
+            enabled = false;
+        }
+    }
+}
