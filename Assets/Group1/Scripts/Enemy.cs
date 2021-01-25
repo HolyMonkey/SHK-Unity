7e85049bc19264532eba8efc48b12d3af3f4c3fd
flangedmace <alex.solov.04@gmail.com>
2021-01-25 20:19:22 +0300

    Revert "Init unity project"
    This reverts commit 3c33d76ba9c0729a3c101b3d98c5397ed987be09.

--- Assets/Group1/Scripts/Enemy.cs
+++ Assets/Group1/Scripts/Enemy.cs
@@ -5,13 +5,5 @@ using UnityEngine;
 [RequireComponent(typeof(EnemyMover))]
 public class Enemy : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.TryGetComponent<Player>(out Player player))
-        {
-            GetComponentInParent<EnemyOwner>().AddKilledEnemy();
 
-            Destroy(gameObject);
-        }
-    }
 }
