412097c16ccdcac630be233fbe8567e978f8714d
Skiedas <>
2021-02-01 22:55:56 +0300

    Code refactoring

---
+++ Assets/Group1/Scripts/Enemy/EnemyMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Vector3 _target;
+
+    private void Start()
+    {
+        SetNextTarget();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        if (transform.position == _target)
+            SetNextTarget();
+    }
+
+    private void SetNextTarget()
+    {
+        _target = Random.insideUnitCircle * 4;
+    }
+}
---
+++ Assets/Group1/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Player))]
+public class PlayerMover : MonoBehaviour
+{
+    private Player _player;
+
+    private void Start()
+    {
+        _player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            Move(Vector3.up);
+
+        if (Input.GetKey(KeyCode.S))
+            Move(Vector3.down);
+
+        if (Input.GetKey(KeyCode.A))
+            Move(Vector3.left);
+
+        if (Input.GetKey(KeyCode.D))
+            Move(Vector3.right);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        transform.position += direction * _player.Speed * Time.deltaTime;
+    }
+}
