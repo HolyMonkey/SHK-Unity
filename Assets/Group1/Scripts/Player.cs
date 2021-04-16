c06ee11b779b61f630443e942abb7f8ba6d6fd17
NemesisWar <adi.beshliev@mail.ru>
2021-04-16 21:31:56 +0300

    Add RequireComponents
    Add RequireComponents in scripts

--- Assets/Group1/Scripts/Player.cs
+++ Assets/Group1/Scripts/Player.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
 }
