a03a0ba7c48a76a420ac92a0151fe71aefbc6687
Pavel Ol <megapiharr2008@gmail.com>
2021-06-14 23:04:53 +0300

    First commit with refactoring

---
+++ Assets/Level/Scripts/General/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Constroller;
+    private const string Message = "SendMEssage";
+
+    [SerializeField] private GameObject _target;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject[] _enemies;
+
+    private void Start()
+    {
+        Constroller = this;
+    }
+
+    public void End()
+    {
+        _target.SetActive(true);
+    }
+
+    private void Update(){
+        foreach (var enemy in _enemies)
+        {
+            if (enemy == null)
+                continue;
+
+                if (Vector3.Distance(_player.gameObject.gameObject.GetComponent<Transform>().position, enemy.gameObject.gameObject.transform.position) < 0.2f)
+                {
+                    _player.SendMessage(Message, enemy);
+                }
+        }
+    }
+}
---
+++ Assets/Level/Scripts/Player/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMover))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private bool _timePassed;
+    [SerializeField] private float _elapsedTime;
+
+    private PlayerMover _playerMover;
+    private const string Enemy = "Enemy";
+    private const string Speed = "speed";
+
+    void Start()
+    {
+        _playerMover = GetComponent<PlayerMover>();
+    }
+
+    private void Update()
+    {
+        if (_timePassed)
+        {
+            _elapsedTime -= Time.deltaTime;
+            if (_elapsedTime < 0)
+            {
+                _timePassed = false;
+                _playerMover.Speed /= 2;
+            }
+        }
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(Enemy);
+
+        if (enemies.Length == 0)
+        {
+            GameController.Constroller.End();
+            enabled = false;
+        }
+    }
+
+    public void SendMEssage(GameObject other)
+    {
+        if(other.name == Enemy)
+        {
+            Destroy(other);
+        }
+        if (other.name == Speed)
+        {
+            _playerMover.Speed *= 2;
+            _timePassed = true;
+            _elapsedTime = 2;
+        }
+    }
+}
