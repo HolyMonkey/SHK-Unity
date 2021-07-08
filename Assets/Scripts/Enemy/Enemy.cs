3834a774beecc7557a4110ea3b4c81e7ab7be26c
Montagne94 <77510722+Montagne94@users.noreply.github.com>
2021-07-09 02:40:03 +0300

    Refactoring v1.0

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    private Vector3 _target;
+    private readonly float _radius = 4f;
+
+    public Vector3 Target => _target;
+
+    private void Start()
+    {
+        _target = Random.insideUnitCircle * _radius;
+    }
+
+    private void Update()
+    {
+        if (transform.position == _target)
+            SetNextTarget();
+    }
+
+    private void SetNextTarget()
+    {
+        _target = Random.insideUnitCircle * _radius;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
