676ce915a56ffaea1d530433cabeca63d6ad9721
Taurus85910 <96919923@mail.ru>
2021-07-15 10:53:44 +1000

    исправление скриптов

---
+++ Assets/Group1/Scripts/EndScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] private EnemyList _enemyList;
+    [SerializeField] private GameObject _endScreen;
+
+    private void OnEnable()
+    {
+        _enemyList.EnemiesDied += OnEnemiesDied;
+    }
+
+    private void OnDisable()
+    {
+        _enemyList.EnemiesDied -= OnEnemiesDied;
+    }
+
+    private void OnEnemiesDied()
+    {
+        _endScreen.SetActive(true);
+    }
+}
---
+++ Assets/Group1/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private int _radiusModifier;
+    [SerializeField] private int _speed;
+    private Vector3 _targetPosition;
+
+    private void Start()
+    {
+        _targetPosition = Random.insideUnitCircle * _radiusModifier;
+    }
+    
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
+        if (transform.position == _targetPosition)
+            _targetPosition = Random.insideUnitCircle * _radiusModifier;
+    }
+}
