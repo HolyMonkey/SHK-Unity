d407882b6c20f64892d748a1cbad3d323d48e8d5
Taurus85910 <96919923@mail.ru>
2021-08-21 00:03:05 +1000

    изменил названия

--- Assets/Group1/Scripts/EnemyMovement.cs
+++ Assets/Group1/Scripts/EnemyMovement.cs
@@ -6,22 +6,22 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private int _radiusModifier;
     [SerializeField] private int _speed;
-    private Vector3 _targetPosition;
+    private Vector3 _target;
 
     private void Start()
     {
-        SetTargetPosition();
+        ChangeTargetPosition();
     }
     
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
-        if (transform.position == _targetPosition) 
-            SetTargetPosition();
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        if (transform.position == _target) 
+            ChangeTargetPosition();
     }
 
-    private void SetTargetPosition()
+    private void ChangeTargetPosition()
     {
-        _targetPosition = Random.insideUnitCircle * _radiusModifier;
+        _target = Random.insideUnitCircle * _radiusModifier;
     }
 }
