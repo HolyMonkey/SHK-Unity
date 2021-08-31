c0fef3fd3f17a299a22545839cd71e68aef920e0
Knockey <BoratEnot@gmail.com>
2021-08-31 20:51:28 +0800

    e refactored into MoveTowardsTarget, code improvements also, some KillAllEnemies scene changes.

---
+++ Assets/Group1/Scripts/MoveTowardsTarget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MoveTowardsTarget : MonoBehaviour
+{
+    [SerializeField] private float _circleRangeAroundTarget;
+    [SerializeField] private float _targetDistanceDelta;
+
+    private Vector3 _target;
+
+    private void Start()
+    {
+        FindNewTarget(_circleRangeAroundTarget);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _targetDistanceDelta * Time.deltaTime);
+
+        if (transform.position == _target)
+            FindNewTarget(_circleRangeAroundTarget);
+    }
+    private void FindNewTarget(float circleRange)
+    {
+        _target = Random.insideUnitCircle * circleRange;
+    }
+}
