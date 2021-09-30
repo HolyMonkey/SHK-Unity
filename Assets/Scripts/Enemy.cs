93cea451386355dc6a67b82339ccaa7a2a625a8b
HAROS <harchenk0s@yandex.ru>
2021-09-30 11:16:51 +0700

    Refactoring code

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _newTargetRadius = 4;
+    [SerializeField] private float _speed = 2;
+
+    private EnemyCounter _enemyCounter;
+    private Vector3 _target;
+    private bool _isBoostBonus;
+
+    public void Init(EnemyCounter enemyCounter)
+    {
+        _enemyCounter = enemyCounter;
+        _enemyCounter.EnemyAdd(this);
+    }
+
+    private void Start()
+    {
+        _isBoostBonus = Random.Range(0, 2) == 1;
+        _target = Random.insideUnitCircle * _newTargetRadius;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        if (transform.position == _target)
+            _target = Random.insideUnitCircle * _newTargetRadius;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Player player;
+
+        if (collision.gameObject.TryGetComponent(out player))
+        {
+            if(_isBoostBonus)
+                player.Boost();
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _enemyCounter.EnemyDown(this);
+    }
+}
---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    [SerializeField] private float _generationRadius = 1;
+    [SerializeField] private int _countEnemies = 4;
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private EnemyCounter _enemyCounter;
+
+    private void Start()
+    {
+        for (int i = 0; i < _countEnemies; i++)
+        {
+            Vector3 position = Random.insideUnitCircle.normalized * _generationRadius;
+            Enemy newEnemy = Instantiate(_enemyPrefab, position, Quaternion.identity, transform).GetComponent<Enemy>();
+            newEnemy.Init(_enemyCounter);
+        }
+    }
+}
