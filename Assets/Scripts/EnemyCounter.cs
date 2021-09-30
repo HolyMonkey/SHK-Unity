553747c489ae63a0f74b50d36653b41f7f23af65
HAROS <harchenk0s@yandex.ru>
2021-09-30 11:30:14 +0700

    Refactoring code, folders, scene

---
+++ Assets/Scripts/EnemyCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCounter : MonoBehaviour
+{
+    [SerializeField] private GameObject _winScreen;
+
+    private List<Enemy> _enemies = new List<Enemy>();
+
+    public void EnemyAdd(Enemy enemy)
+    {
+        _enemies.Add(enemy);
+    }
+
+    public void EnemyDown(Enemy enemy)
+    {
+        _enemies.Remove(enemy);
+
+        if (_enemies.Count == 0)
+            End();
+    }
+
+    private void End()
+    {
+        _winScreen.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _boostTime;
+
+    public void Boost()
+    {
+        StartCoroutine(BoostCoroutine());
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 moveVector = Vector2.zero;
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y = Input.GetAxis("Vertical");
+
+        transform.Translate(moveVector * _speed * Time.deltaTime);
+    }
+
+    private IEnumerator BoostCoroutine()
+    {
+        _speed *= 2;
+        yield return new WaitForSecondsRealtime(_boostTime);
+        _speed /= 2;
+    }
+}
