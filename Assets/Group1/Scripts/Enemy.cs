5b8c60b89d00665d7263d05ee165ace6d232dfe7
Gloomi13 <Gloomi13@mail.ru>
2022-08-31 15:24:45 +1000

    Refactoring
    Изменена система управления
    Добавлен бонус ускорения
    Изменены именование переменных и методов
    Созданы Префабы

---
+++ Assets/Group1/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _radius = 4.0f;
+    [SerializeField] private float _speed = 2.0f;
+    private Vector3 _target;
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        ChangeTarget();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        if (transform.position == _target)
+            ChangeTarget();
+    }
+
+    private void ChangeTarget()
+    {
+        _target = Random.insideUnitCircle * _radius;
+    }
+
+}
---
+++ Assets/Group1/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _timeBoost = 2.0f;
+    [SerializeField] private float _powerSpeedBoost = 2.0f;
+
+    private float _maxSpeed;
+    private float _minSpeed;
+    private float _horizontalMove;
+    private float _verticalMove;
+    private float _temporaryEffect;
+
+    public static UnityEvent EnemyDefeated = new UnityEvent();
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            enemy.Death();
+            EnemyDefeated.Invoke();
+        }
+
+        if (collider.TryGetComponent<SpeedBoost>(out SpeedBoost speedBoost))
+        {
+            _temporaryEffect = _timeBoost;
+            _speed = _maxSpeed;
+        }
+    }
+
+    private void Start()
+    {
+        _maxSpeed = _speed * _powerSpeedBoost;
+        _minSpeed = _speed;
+    }
+
+    private void Update()
+    {
+        _temporaryEffect -= Time.deltaTime;
+        if (_temporaryEffect < 0)
+            _speed = _minSpeed;
+
+        _horizontalMove = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        _verticalMove = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        transform.Translate(_horizontalMove, _verticalMove, 0);
+    }
+}
